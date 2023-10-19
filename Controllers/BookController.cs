ad9a26a5f6c1d27a1725b539f5c642d69efc5241
EmirhanBayramoglu <emirhan_bayramoglu@hotmail.com>
2023-10-19 17:51:22 +0300

    Bazı HttpGet işlemleri eklendi Nasıl birden fazla get işlemi yapılabiliniyor açıklandı ApplicationContext Oluşturuldu

--- Controllers/BookController.cs
+++ Controllers/BookController.cs
@@ -1,11 +1,37 @@
-﻿using Microsoft.AspNetCore.Http;
+﻿using bookDemo.Data;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
 namespace bookDemo.Controllers
 {
-    [Route("api/[controller]")]
+    //ApiController bize hata bildirimlerinde falan kolayca bilgi fırlatmasını sağlar o yüzden eklenmesi çok önemlidir
+    [Route("api/books")]
     [ApiController]
     public class BookController : ControllerBase
     {
+        [HttpGet]
+        public IActionResult GetAllBook()
+        {
+            var books = ApplicationContext.Books;
+            return Ok(books);
+        }
+
+        //Aynı anda 2 tane get direkt olarak kullanılamaz o yüzden parametre kullanıyoruz
+        //FromeRoute eklenmesi mecbur birşey değildir ama bazı karışıklıkları eklenebilir
+        [HttpGet("{id:int}")]
+        public IActionResult GetOneBook([FromRoute(Name ="id")] int id)
+        {
+            var book = ApplicationContext.
+                Books.Where(x => x.Id.Equals(id)).SingleOrDefault();
+            //SingleOrDefault LINQ içerisinde arama metodudur (sadece 1 tane olanı getir yada defualt değeri döndür demek)
+
+            if (book == null)
+            {
+                return NotFound();
+            }
+            return Ok(book);
+        }
+
+
     }
 }
---
+++ Data/ApplicationContext.cs
@@ -0,0 +1,23 @@
+﻿using bookDemo.Models;
+
+
+namespace bookDemo.Data
+{
+    //static oluşturuyoruz ki buradaki değişikliklere anlık olarak her yerden ulaşılabilinsin
+    //*staticler newlenemez
+    public static class ApplicationContext
+    {
+        public static List<Book> Books { get; set; }
+
+        static ApplicationContext()
+        {
+            Books = new List<Book>()
+            {
+                new Book(){Id = 1, Title = "Mesnevi", Price = 10},
+                new Book(){Id = 2, Title = "Masal Masal icinde", Price = 150},
+                new Book(){Id = 3, Title = "The Walking Dead", Price = 35}
+            };
+        }
+
+    }
+}
